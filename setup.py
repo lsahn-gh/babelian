c425c696457c516ee5982626fe87302fc5fb9904
memnoth <yisooan@gmail.com>
2016-12-27 19:12:58 +0900

    Edit some description (setup.py)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Babelian
 --------
 
-The Babelian is a dictionary of a Terminal.
+Babelian is a dictionary of a Terminal.
 You can search some words/phrases/examples without Browser.
 This program will help Developers/Students/Teachers,
 or who is learning 2nd languages.
@@ -11,8 +11,8 @@ Thanks **glosbe**!
 
 Links
 `````
-* Guide  : https://github.com/cryptosan/babelian
-* Github : https://github.com/cryptosan/babelian
+* Guide  : https://github.com/memnoth/babelian
+* Github : https://github.com/memnoth/babelian
 """
 # -*- coding: utf-8 -*-
 import os
@@ -29,11 +29,11 @@ __ver__ = ver_line[0].split('=')[1].strip().replace('\'', '')
 setup(
     name='Babelian',
     version=__ver__,
-    url='https://github.com/cryptosan/babelian',
+    url='https://github.com/memnoth/babelian',
     license='MIT',
-    author='Cryptos An',
-    author_email='frostlabx@gmail.com',
-    description='The Babelian is a dictionary of a Terminal.',
+    author='Yi Soo, (Jeff) An',
+    author_email='yisooan@gmail.com',
+    description='Babelian is a dictionary of a Terminal.',
     long_description=__doc__,
     zip_safe=False,
     include_package_data=True,
